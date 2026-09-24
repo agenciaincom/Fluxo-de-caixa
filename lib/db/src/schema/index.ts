1f2074aa8e0c66b5593af58804b4a058f99439a8
agenciaincom <gege.georgia@gmail.com>
2026-09-24 18:48:10 -0300

    Update index.ts

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -2,3 +2,6 @@ export * from "./entradas";
 export * from "./saidas";
 export * from "./subscriptions";
 export * from "./conciliacao";
+export * from "./centrosCusto";
+export * from "./lembretes";
+export * from "./atividade";
